e4c70a67e836596d3f10c092dcbfa8713ed79137
Cloudef <mailRoxas@gmail.com>
2012-04-27 17:12:17 +0300

    Fix wrong define use

--- monsterwm.c
+++ monsterwm.c
@@ -236,7 +236,7 @@ client* addwindow(Window w) {
     else if (t) t->next = c; else head->next = c;
 
     c->monitor = current_monitor;
-    XSelectInput(dis, (c->win = w), PropertyChangeMask|FocusChangeMask|(FOLLOW_MOUSE?EnterWindowMask:0));
+    XSelectInput(dis, (c->win = w), PropertyChangeMask|FocusChangeMask|(FOLLOW_MONITOR?EnterWindowMask:0));
     return c;
 }
 
@@ -1127,7 +1127,7 @@ void xinerama_magic(void) {
 /* motion notify event */
 void motionnotify(XEvent *e) {
     int m;
-    if (FOLLOW_MOUSE &&
+    if (FOLLOW_MONITOR &&
        (m = areatomonitor(e->xmotion.x_root, e->xmotion.y_root)) != current_monitor)
         change_monitor(&(Arg){.i = m});
 }
